3421031564e909c4efc7a7cc5be36b380a066ad1
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-02-07 06:36:06 +0530

    modified:   src/components/Form/Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import './Form.css';
 import { useState } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs';
 import 'react-toastify'
 import { ToastContainer, toast } from 'react-toastify';
 const Form = () => {
